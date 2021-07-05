21749afb18aa805fbf84ab9e6772eec8f3331104
Kevin_Burke <kburketx@verizon.net>
2021-07-05 18:47:14 -0500

    Updated runme.cmd to create HTML file from Markdown

--- runme.cmd
+++ runme.cmd
@@ -1,2 +1,3 @@
 pandoc --to=docx --from=markdown --output=ReplaysDB_kburketx.docx ReplaysDB_kburketx.md
 pandoc --to=pdf --from=markdown --output ReplaysDB_kburketx.pdf ReplaysDB_kburketx.md
+pandoc --to=html --from=markdown --output ReplaysDB_kburketx.html ReplaysDB_kburketx.md
